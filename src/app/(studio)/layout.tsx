ba864df6cf2a7d95b495b75a1cf218f06ce10211
claropromweb <claropromweb@gmail.com>
2026-09-05 21:50:23 +0200

    Add Sanity Dashboard bridge to embedded Studio

--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -1,11 +1,20 @@
+import { preloadModule } from 'react-dom'
+
+const bridgeScript = 'https://core.sanity-cdn.com/bridge.js'
+
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	preloadModule(bridgeScript, { as: 'script' })
+
 	return (
 		<html lang="en">
-			<body style={{ margin: 0 }}>{children}</body>
+			<body style={{ margin: 0 }}>
+				<script src={bridgeScript} async type="module" />
+				{children}
+			</body>
 		</html>
 	)
 }
