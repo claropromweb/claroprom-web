683af4e43d6423d77cdfac91b94db631e2b000b3
claropromweb <claropromweb@gmail.com>
2026-09-06 10:20:47 +0200

    Add Sanity CLI environment fallbacks

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,8 @@
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+export const projectId =
+	process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'ddtwki7e'
+
+export const dataset =
+	process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export const apiVersion =
 	process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-06-17'
